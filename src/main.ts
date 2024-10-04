cb4ef45aad0c84b088b26ccc452c50cc68e1a0b3
Thomas Wessel <turpleturtle12@gmail.com>
2024-10-03 17:28:28 -0700

    Create Phaser project

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { scene1 } from './scenes/scene1';
+
+import { Game, Types } from "phaser";
+
+//  Find out more information about the Game Config at:
+//  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
+const config: Types.Core.GameConfig = {
+    type: Phaser.AUTO,
+    width: 1024,
+    height: 768,
+    parent: 'game-container',
+    backgroundColor: '#127803',
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    scene: [
+        scene1
+    ]
+};
+
+export default new Game(config);
---
+++ src/scenes/scene1.ts
@@ -0,0 +1,17 @@
+import {Scene} from 'phaser';
+
+export class scene1 extends Scene {
+
+
+
+    constructor() {
+        super('scene1');
+    }
+
+    preload() {
+    }
+
+    create() {
+
+    }
+}
